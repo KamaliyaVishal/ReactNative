5a0484f49b1d9f38540db3c445c75f7a578803de
Vishal <kamaliyavishal96@gmail.com>
2020-08-14 08:55:09 +0530

    Assignment 1: React Native Components and Navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,8 @@ import Dishdetail from "./DishdetailComponent";
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu },
@@ -37,6 +39,35 @@ const HomeNavigator = createStackNavigator({
     })
 });
 
+
+const ContactNavigator = createStackNavigator({
+    Contact: { screen: Contact }
+}, {
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: "currentcolor"
+        },
+        headerTitleStyle: {
+            color: "#fff"
+        },
+        headerTintColor: "#fff"
+    })
+});
+
+const AboutNavigator = createStackNavigator({
+    About: { screen: About }
+}, {
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: "currentcolor"
+        },
+        headerTitleStyle: {
+            color: "#fff"
+        },
+        headerTintColor: "#fff"
+    })
+});
+
 const MainNavigator = createDrawerNavigator({
     Home:
     {
@@ -46,12 +77,28 @@ const MainNavigator = createDrawerNavigator({
             drawerLabel: 'Home'
         }
     },
+    Aboutus:
+    {
+        screen: AboutNavigator,
+        navigationOptions: {
+            title: 'About Us',
+            drawerLabel: 'About Us'
+        }
+    },
     Menu:
     {
         screen: MenuNavigator,
         navigationOptions: {
             title: 'Menu',
             drawerLabel: 'Menu'
+        },
+    },
+    contactus:
+    {
+        screen: ContactNavigator,
+        navigationOptions: {
+            title: 'Contact Us',
+            drawerLabel: 'Contact Us'
         },
     }
 }, {
